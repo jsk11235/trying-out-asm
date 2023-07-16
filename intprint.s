90692554a8ae9974764baf322507d20a6c3074d3
jsk11235 <kirmayerjacob@gmail.com>
2023-07-16 18:52:33 -0400

    intprint working, primitive memory-wasting branching

--- intprint.s
+++ intprint.s
@@ -2,26 +2,48 @@
 .align 2
 
 _start:
-    movz    w10, 1           
-    movk    w10, 1, LSL #16  
+    movz    w10, 0xAAB           
+    movk    w10, 0xAAA, LSL #16  
 
-    mov w11, 10
-    mov w14, 0
-    bl intprint
+    mov w11, 16 // store the base in w11
+    mov w14, 0 
+    bl _loadretaddr // set return address to next instruction
+    add X30, X30, 20 // the return address is currently at this instruction, move it down by 5 instructions
+    stp X29, X30, [sp, -8] // store frame pointer to prev function and return address
+    sub X29, sp , 8 // store the address of the old fp in the new fp
+    sub sp, sp, 16 // move the stack pointer down to block space reserved for fp and lr
+    b intprint
+    sub sp, sp, 16
+    mov w4, 10
+    mov w5, 10
+    strb w4, [sp]
+    strb w5, [sp,1]
+    mov X1, sp
+    mov X2, 2
+    bl _print
     b _end
+
+
+_loadretaddr:
+    ret
 
 
 // load number into w10 and base into w11 before invoking
 intprint:
-    stp X29, X30, [sp, -8] // store frame pointer to prev function and return address
-    sub X29, sp , 8
-    sub sp, sp, 16
+    // store the return address to be used by next function call
+    bl _loadretaddr // set return address to next instruction
+    add X30, X30, 28 // the return address is currently at this instruction, move it down by 7 instructions
+    stp X29, X30, [sp, -8] // store frame pointer to this function and return address
+    sub X29, sp , 8 // store the address of the old fp in the new fp
+    sub sp, sp, 16 // move the stack pointer down to block space reserved for fp and lr
+
+
     sub X15, sp , 1
     mov X2, 0
-    bl _looproutine
+    b _looproutine
+    // fp will point to location of intprint's return address and associated fp after looproutine is called
     mov X1, X15 // Load X15 (address of char pointer) into X1
     bl _print
-    sub lr, lr, 20
     ldp X29, X30, [X29]
     ret
 
@@ -29,6 +51,14 @@ _looproutine:
     udiv    w12, w10, w11
 
     msub    w13, w12, w11, w10
+
+    cmp w13, 10
+
+    bl _loadretaddr
+
+    add x30, x30, 8 // return address 2 instructions after this one
+
+    bcs _alphabeticadd 
 
     add w13 , w13, 48
 
@@ -40,7 +70,11 @@ _looproutine:
 
     add X2 , X2 , 1
     cbnz w12, _looproutine
+    ldp X29, X30, [X29]
     ret
+
+_alphabeticadd:
+    add w13 , w13, 7
 
 _print:
     mov X16, 4
